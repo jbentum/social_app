b8ea52e8b208634c17c33330de206b9d005c843a
jbentum <jefferybentum17@gmail.com>
2023-12-11 09:03:42 +0000

    I did the authentication for the sign that lead to the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_app/firebase_options.dart';
+import 'package:social_app/services/auth/auth_gate.dart';
+import 'package:social_app/services/auth/auth_service.dart';
 
-import 'services/auth/login_or_register.dart';
-
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: "AIzaSyClbuIpx53b-6coQzD59eeEXUnFiSvu8n8",
+            appId: "1:367896227686:android:63f9de1a5dc276923622a7",
+            messagingSenderId: "367896227686",
+            projectId: "chatapp-3e1e4",
+          ),
+        )
+      : await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthService(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +35,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:social_app/components/my_button.dart';
 import 'package:social_app/components/my_text_field.dart';
+import 'package:social_app/services/auth/auth_service.dart';
 
 class LoginPage extends StatefulWidget {
   final void Function()? onTap;
@@ -17,7 +19,27 @@ class _LoginPageState extends State<LoginPage> {
 
   // sign in user
 
-  void signIn() {}
+  void signIn() async {
+    // get the auth service
+    final authService = Provider.of<AuthService>(context, listen: false);
+
+    try {
+      await authService.signInWithEmailandPassword(
+        emailController.text,
+        passwordController.text,
+      );
+    } catch (e) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.toString(),
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,81 +48,83 @@ class _LoginPageState extends State<LoginPage> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-                //logo
-                Icon(
-                  Icons.message,
-                  size: 100,
-                  color: Colors.grey[800],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
+                  //logo
+                  Icon(
+                    Icons.message,
+                    size: 100,
+                    color: Colors.grey[800],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-                //Welcome message
-                const Text(
-                  'Welcome back, you\'ve been missed',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
+                  //Welcome message
+                  const Text(
+                    'Welcome back, you\'ve been missed',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-                //email textfields
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email',
-                  obscureText: false,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
+                  //email textfields
+                  MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
 
-                //password textfields
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email',
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
+                  //password textfields
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-                // sign in button
-                MyButton(
-                  onTap: signIn,
-                  text: 'Sign In',
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
+                  // sign in button
+                  MyButton(
+                    onTap: signIn,
+                    text: 'Sign In',
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
 
-                //not a member? register now
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Not a member?'),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child: const Text(
-                        'Register now',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                  //not a member? register now
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Not a member?'),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: const Text(
+                          'Register now',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
